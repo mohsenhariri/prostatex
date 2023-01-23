e406977a6f5a9114543d4757a76acc66f6f658f7
Mohsen Hariri <mhariri68@gmail.com>
2023-01-22 21:16:21 -0500

    add absolute path support

--- pkg/utility/reader.py
+++ pkg/utility/reader.py
@@ -3,12 +3,15 @@ from pathlib import Path
 
 from .custom_type import PathType
 
-root_in = getenv("ROOT_IN") or "."
-root_out = getenv("ROOT_OUT") or "."
-
 
 def path_in(p: str) -> PathType:
-    path: PathType = Path(rf"{root_in}/{p}")
+    if p[0] == "/":  # Check whether p is absolute path.
+        path_input = p
+    else:
+        root_in = getenv("ROOT_IN") or "."
+        path_input = rf"{root_in}/{p}"
+
+    path: PathType = Path(path_input)
     if not path.exists():
         raise Exception(f"{p} doesn't exist on the earth.")
 
@@ -16,7 +19,13 @@ def path_in(p: str) -> PathType:
 
 
 def path_out(p: str) -> PathType:
-    path: PathType = Path(rf"{root_out}/{p}")
+    if p[0] == "/":  # Check whether p is absolute path
+        path_output = p
+    else:
+        root_out = getenv("ROOT_OUT") or "."
+        path_output = rf"{root_out}/{p}"
+
+    path: PathType = Path(path_output)
     if not path.exists():
         print("The output path doesn't exist but no worries I'm making it.")
         path.mkdir(parents=True)
